b4c51ff19aaff1308a0aab17d735db89c959544f
jilanAblah139 <jilanablahhanifah97@gmail.com>
2024-11-23 19:18:33 +0700

    added retrofit dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     //retrofit
     implementation(libs.glide)
     implementation(libs.retrofit)
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+    implementation(libs.converter.gson)
+    implementation(libs.logging.interceptor)
+
 }
