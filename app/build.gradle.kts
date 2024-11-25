7151539f87cb2436e27ddfd15b5fef1b8e006eb3
qitqath <qilahqathxx@gmail.com>
2024-11-25 09:44:27 +0700

    add dark mode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,9 +48,18 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.circleimageview)
+
+    implementation(libs.androidx.datastore.preferences)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx.v261)
+    implementation(libs.androidx.lifecycle.livedata.ktx.v261)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
     implementation(libs.androidx.legacy.support.v4)
     implementation(libs.androidx.fragment.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(kotlin("script-runtime"))
 }
--- app/src/main/java/com/md/kebunq/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/md/kebunq/ui/settings/SettingsViewModel.kt
@@ -1,13 +1,35 @@
 package com.md.kebunq.ui.settings
 
+import android.app.Application
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 
-class SettingsViewModel : ViewModel() {
+class SettingsViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is Settings Fragment"
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+
+    private val DARK_MODE_KEY = booleanPreferencesKey("dark_mode")
+
+    val isDarkMode: LiveData<Boolean> = application.dataStore.data
+        .map { preferences ->
+            preferences[DARK_MODE_KEY] ?: false
+        }.asLiveData()
+
+    fun saveDarkModeSetting(isDarkMode: Boolean) {
+        viewModelScope.launch {
+            getApplication<Application>().dataStore.edit { settings ->
+                settings[DARK_MODE_KEY] = isDarkMode
+            }
+        }
     }
-    val text: LiveData<String> = _text
 }
