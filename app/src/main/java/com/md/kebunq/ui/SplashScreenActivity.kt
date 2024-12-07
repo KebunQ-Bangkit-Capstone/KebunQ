a89bbe7f8e221f27dcae61121a78eeb0564fdadc
jilanAblah139 <2210511139@mahasiswa.upnvj.ac.id>
2024-12-07 22:33:31 +0700

    add welcome and splashscreen

--- app/src/main/java/com/md/kebunq/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/md/kebunq/ui/SplashScreenActivity.kt
@@ -1,21 +1,21 @@
 package com.md.kebunq.ui
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.md.kebunq.R
+import com.md.kebunq.ui.welcome.WelcomeActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent =  Intent(this, WelcomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 15000)
     }
 }
