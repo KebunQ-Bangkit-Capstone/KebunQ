abf81b3df6833c22a84d608fb83ce2e2b39cfbae
jilanAblah139 <2210511139@mahasiswa.upnvj.ac.id>
2024-12-11 21:59:07 +0700

    handle timeout

--- app/src/main/java/com/md/kebunq/data/retrofit/ApiConfig.kt
+++ app/src/main/java/com/md/kebunq/data/retrofit/ApiConfig.kt
@@ -4,6 +4,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class ApiConfig {
     companion object {
@@ -14,6 +15,10 @@ class ApiConfig {
             
             val client = OkHttpClient.Builder()
                 .addInterceptor(loggingInterceptor)
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .writeTimeout(60, TimeUnit.SECONDS)
+                .readTimeout(60, TimeUnit.SECONDS)
+                .retryOnConnectionFailure(true)
                 .build()
                 
             val retrofit = Retrofit.Builder()
